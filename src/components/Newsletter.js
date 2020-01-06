b3cd53d3a49edd2657b133b83f890c0e71658542
Isaac Martin <isaac.martin@fanduel.com>
2020-01-06 10:32:34 -0500

    added newsletter

---
+++ src/components/Newsletter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Newsletter = () => (
+  <form>
+    <input type="email"></input>
+    <button type="submit">Submit</button>
+  </form>
+)
+
+export default Newsletter
